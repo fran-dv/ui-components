9170a77a2e78fa4fa61c23daacb2f2b47c5179ea
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-04-23 16:09:52 -0300

    feat: a simple and reusable dropdown component

---
+++ src/components/dropdown/Dropdown.ts
@@ -0,0 +1,42 @@
+import './dropdown.css';
+
+interface DropdownProps {
+    container: HTMLDivElement,
+    button: HTMLButtonElement,
+    content: HTMLDivElement,
+}
+
+const enum Classes {
+    container = 'dropdown',
+    button = 'dropdown-btn',
+    content = 'dropdown-content',
+    hidden = 'hidden',
+}
+
+const toggleVisibility = (content: HTMLDivElement) => {
+    content.classList.toggle(Classes.hidden);
+}
+
+export const createDropdown = ({ container , button, content }: DropdownProps) => {
+    container.classList.add(Classes.container)
+    button.classList.add(Classes.button)
+    content.classList.add(Classes.content, Classes.hidden)
+
+    const handleClick = () => toggleVisibility(content)
+    const handleBlur = () => {
+        if (!content.classList.contains(Classes.hidden)){
+            toggleVisibility(content);
+        }
+    }
+
+    button.addEventListener('click', handleClick)
+    button.addEventListener('blur', handleBlur)
+
+
+    return {
+        destroy() { 
+            button.removeEventListener('click', handleClick)
+            button.removeEventListener('blur', handleBlur)
+        }
+    }
+}
---
+++ usage-examples/dropdown/dropdown-example.ts
@@ -0,0 +1,14 @@
+import './style.css'
+import { createDropdown } from '../../src/components/'
+
+const container = document.querySelector('#favourite-food') as HTMLDivElement;
+const button = document.querySelector('.food-btn')as HTMLButtonElement;
+const content = document.querySelector('#food-options') as HTMLDivElement;
+
+const Dropdown = createDropdown({ container, button, content });
+
+const destroyDropdown = false;
+
+if (destroyDropdown) {
+    Dropdown.destroy()
+}
