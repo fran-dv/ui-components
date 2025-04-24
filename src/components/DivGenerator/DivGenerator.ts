5c8abce78b63cca228833589c99e2ec82cde0436
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-04-24 17:45:53 -0300

    feat: add div element generator compnent

---
+++ src/components/DivGenerator/DivGenerator.ts
@@ -0,0 +1,35 @@
+interface CustomDataAttr {
+    name: string; // pass it without data- prefix. It must be camelCase or kebab-case
+    value: string;
+}
+
+interface DivProps {
+    classes?: string[];
+    id?: string;
+    customAttrs?: CustomDataAttr[]; 
+}
+
+export const generateDiv = ({ classes, id, customAttrs }: DivProps = {}): HTMLDivElement => {
+    const div = document.createElement('div')
+
+    if (classes) {
+        div.classList.add(...classes)
+    }
+
+    if (id) {
+        div.id = id;
+    }
+
+    if (customAttrs) {
+        customAttrs.forEach((attr) => {
+            if (/\s/.test(attr.name)) {
+                console.error(`'${attr.name}' is not valid. It can't contain spaces`)
+                return;
+            }
+
+            div.dataset[attr.name] = attr.value;
+        })
+    }
+
+    return div;
+}
